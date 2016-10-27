eb9a555b43035163a94147f11925d174f77106ba
Miguel Gonzalez <migonzalvar@gmail.com>
2016-10-27 19:13:32 +0200

    Pone en español y hora Madrid

--- alpha/settings.py
+++ alpha/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
