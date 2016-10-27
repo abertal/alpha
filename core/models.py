b3e6727f26ed591909335487df40e850f023e97c
Miguel Gonzalez <migonzalvar@gmail.com>
2016-10-27 18:52:58 +0200

    Crea modelo Person

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    name = models.TextField(verbose_name='Nombre')
+    surname = models.TextField(verbose_name='Apellidos')
+    birthday = models.DateField(verbose_name='Fecha de nacimiento')
+    id_number = models.TextField(verbose_name='DNI/NIE')
+    ss_number = models.TextField(verbose_name='Tarjeta sanitaria')
+    address_street = models.TextField(verbose_name='Dirección')
+    address_locality = models.TextField(verbose_name='Localidad')
+    address_region = models.TextField(verbose_name='Provincia')
+    address_country = models.TextField(verbose_name='País')
+    phone_number = models.TextField(verbose_name='Teléfono')
+    email = models.EmailField(verbose_name='Correo electrónico')
+    comment = models.TextField(verbose_name='Observaciones')
+    health_warnings = models.TextField(verbose_name='Observaciones médicas')
