eaac51ce64b976c4747d7ddbee6300391090c08b
OniOrge <stfnorge@gmail.com>
2016-11-04 10:37:21 +0100

    Nueva clase intermedia grupo-persona

--- core/models.py
+++ core/models.py
@@ -35,3 +35,7 @@ class Group(models.Model):
 
     def __str__(self):
         return self.group_name
+
+class PersonGroup(models.Model):
+    person_id = models.ForeignKey(Person)
+    group_id = models.ForeignKey(Group)
