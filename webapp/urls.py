a89ce4b4a8b1d1abcedd6ac29ae9d630ca14d2c0
gvame <gvarelamendez@gmail.com>
2016-11-24 11:48:58 +0100

    Vistas detalle grupo, lista usuarios,detalle usuarios

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.group_list),
+    url(r'^$', views.group_list), 
+    url(r'^groups/(?P<pk>\d+)/$', views.group_detail), 
+    url(r'^person/', views.person_list),
+    url(r'^person/(?P<pk>\d+)/$',views.person_detail),
+    
 ]
