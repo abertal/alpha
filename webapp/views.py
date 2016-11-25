1987293d1c5e8884f2c458ffe36cf906f64adede
gvame <gvarelamendez@gmail.com>
2016-11-25 18:12:40 +0100

    Añade vistas de grupo y persona
    * resubida simple template
    
    * Vistas detalle grupo, lista usuarios,detalle usuarios
    
    * Enlaces de lista a detalle
    
    * correccion sintaxis
    
    * correccion sintaxis2
    
    * correccion sintaxis3
    
    * correccionSintaxis4

--- webapp/views.py
+++ webapp/views.py
@@ -7,3 +7,21 @@ def group_list(request):
     object_list = models.Group.objects.all()
     context = {'object_list': object_list}
     return render(request, 'webapp/main.html', context=context)
+
+
+def group_detail(request, pk):
+    object = models.Group.objects.get(pk=pk)
+    context = {'object': object}
+    return render(request, 'webapp/detail.html', context=context)
+
+
+def person_list(request):
+    object_list = models.Person.objects.all()
+    context = {'object_list': object_list}
+    return render(request, 'webapp/person_list.html', context=context)
+
+
+def person_detail(request, pk):
+    object = models.Person.objects.get(pk=pk)
+    context = {'object': object}
+    return render(request, 'webapp/person_detail.html', context=context)
