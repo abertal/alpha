722ec89bdd03ec05faa0a35ace71c511839f169f
gvame <gvarelamendez@gmail.com>
2016-11-28 19:14:27 +0100

    Arreglo de redireccionamiento Person

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.group_list),
     url(r'^groups/(?P<pk>\d+)/$', views.group_detail, name='group_detail'),
-    url(r'^person/', views.person_list),
+    url(r'^person/$', views.person_list),
     url(r'^person/(?P<pk>\d+)/$', views.person_detail, name='person_detail'),
 ]
