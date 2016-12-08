c82d4068f5111c6bd15139572c8781e6e9b8ccb3
Miguel Gonzalez <migonzalvar@gmail.com>
2016-12-08 19:21:13 +0100

    Abre archivo en modo binario

--- core/management/commands/importdata.py
+++ core/management/commands/importdata.py
@@ -7,7 +7,10 @@ class Command(BaseCommand):
     help = 'Importa datos desde un archivo.'
 
     def add_arguments(self, parser):
-        parser.add_argument('filename', type=argparse.FileType('r'))
+        parser.add_argument('filename', type=argparse.FileType('rb'))
 
     def handle(self, *args, **options):
+        fp = options['filename']
+        for _ in fp:
+            print('.', end='')
         self.stdout.write(self.style.SUCCESS('Importación finalizada con éxito.'))
