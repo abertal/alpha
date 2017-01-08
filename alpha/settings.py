f1a891964d2cc6059a45be2b721bc770107d39aa
Miguel Gonzalez <migonzalvar@gmail.com>
2017-01-08 18:05:40 +0100

    Configure ALLOWED_HOSTS

--- alpha/settings.py
+++ alpha/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'u*#m!mlfnn8$#z8w&o^cvee6uqzmmmjz3ivtbm3!7nx90!684c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '[::1]',
+    '.herokuapp.com',
+]
 
 
 # Application definition
