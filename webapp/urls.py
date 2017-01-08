1938cc42c7908140659cf2bac15f354ccd75e8be
Miguel Gonzalez <migonzalvar@gmail.com>
2017-01-08 21:15:20 +0100

    Corrige expresión regular de URL detalle de persona
    Ya no es un número sino un UUID.

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', views.group_list),
     url(r'^groups/(?P<pk>\d+)/$', views.group_detail, name='group_detail'),
     url(r'^person/$', views.person_list),
-    url(r'^person/(?P<pk>\d+)/$', views.person_detail, name='person_detail'),
+    url(r'^person/(?P<pk>[^/]+)/$', views.person_detail, name='person_detail'),
     url(r'^missing_doc/$', views.missing_doc),
 ]
