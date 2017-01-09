9c2db13b7220b3a979b85069ac04d5fdc2c27edd
Miguel Gonzalez <migonzalvar@gmail.com>
2017-01-09 23:06:08 +0100

    Pone nombre a las vistas

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.group_list),
     url(r'^groups/(?P<pk>\d+)/$', views.group_detail, name='group_detail'),
-    url(r'^person/$', views.person_list),
+    url(r'^person/$', views.person_list, name='person_list'),
     url(r'^person/(?P<pk>[^/]+)/$', views.person_detail, name='person_detail'),
-    url(r'^missing_doc/$', views.missing_doc),
+    url(r'^missing_doc/$', views.missing_doc, name='missing_doc'),
 ]
