4405285ab88bb8585758082d9be7ac94dd4460ea
gvame <gvarelamendez@gmail.com>
2017-01-19 10:12:14 +0100

    blank line

--- webapp/tests/test_views.py
+++ webapp/tests/test_views.py
@@ -15,6 +15,7 @@ def test_list_views(view_name):
     response = c.get(url)
     assert response.status_code == 200
 
+
 @pytest.mark.django_db
 @pytest.mark.parametrize('url', [
     '/webapp/login/',
