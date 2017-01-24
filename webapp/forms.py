a3ea5f9f5a0ab8a69d179f73f4dc02071bb15aec
gvame <gvarelamendez@gmail.com>
2017-01-24 12:07:01 +0100

    añade mas campos para guardar en la bd + añade campo dni al formulario

--- webapp/forms.py
+++ webapp/forms.py
@@ -5,12 +5,18 @@ from core.models import Person
 
 class NewIndividualMember(forms.Form):
     name = forms.CharField()
+    surname = forms.CharField()
     phone = forms.CharField()
+    adress = forms.CharField()
+    mail = forms.CharField()
+    id_number = forms.CharField()
+
+
 
     def execute(self):
         cleaned_data = self.cleaned_data
         print(cleaned_data)
-        person = Person.objects.create(name=cleaned_data['name'])
+        person = Person.objects.create(name=cleaned_data['name'], surname=cleaned_data['surname'], phone_number=cleaned_data['phone'], address_street=cleaned_data['adress'], id_number=cleaned_data['id_number'], email=cleaned_data['mail'])
         print(person)
 
 
