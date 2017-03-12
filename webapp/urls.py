523027bd567f13ac998e2a357976c4e1a48749ef
Miguel Gonzalez <migonzalvar@gmail.com>
2017-03-12 21:08:20 +0100

    Reorganiza urls con include

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,13 +1,20 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.views.generic import RedirectView
 
 from . import views
 
+
+person = [
+    url(r'^$', views.PersonList.as_view(), name='person-list'),
+    url(r'^(?P<pk>[^/]+)/$', views.PersonDetail.as_view(), name='person-detail'),
+    url(r'^(?P<pk>[^/]+)/edit/$', views.PersonEdit.as_view(), name='person-edit'),
+]
+
+
 urlpatterns = [
+    url(r'^person/', include(person)),
     url(r'^$', RedirectView.as_view(url='login/')),
     url(r'^groups/(?P<pk>\d+)/$', views.group_detail, name='group_detail'),
-    url(r'^person/$', views.PersonList.as_view(), name='person-list'),
-    url(r'^person/(?P<pk>[^/]+)/$', views.PersonDetail.as_view(), name='person-detail'),
     url(r'^missing_doc/$', views.missing_doc, name='missing_doc'),
 
     url(r'^login/$', views.login),
