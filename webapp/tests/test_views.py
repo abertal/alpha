98a9a23aa0afe6e4181d0567373adeeeb20a96f5
Ivan Nieto <ivan.n.s@tuta.io>
2017-03-24 11:48:08 +0100

    Repara test login

--- webapp/tests/test_views.py
+++ webapp/tests/test_views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import reverse
 from django.test import Client
+from core import models
+from django.contrib import auth
 
 import pytest
 
@@ -35,9 +37,11 @@ def test_views_exist(url):
 @pytest.mark.parametrize('url', [
     '/webapp/login/',
 ])
-def test_views_post_and_redirect(url):
+def test_views_post_and_redirect(url, django_user_model):
+    from django.contrib.auth.models import User
+    user = User.objects.create_user('admin', 'admin@example.com', '12345678')
     c = Client()
-    response = c.post(url)
+    response = c.post(url, {'user':'admin', 'password':'12345678'})
     assert response.status_code == 302
 
 
@@ -48,6 +52,7 @@ def test_views_post_and_redirect(url):
 ])
 def test_views_post_with_errors(url):
     c = Client()
+    print(c)
     response = c.post(url)
     assert response.status_code == 200
 
