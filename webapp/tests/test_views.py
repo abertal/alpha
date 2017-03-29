a95da524fdc3c3c6ae9f93b73ed81f866884f0e4
Miguel Gonzalez <migonzalvar@gmail.com>
2017-03-29 10:44:38 +0200

    Elimina import no usado

--- webapp/tests/test_views.py
+++ webapp/tests/test_views.py
@@ -1,7 +1,4 @@
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-from django.contrib.auth import login
-from django.http import HttpRequest
 from django.shortcuts import reverse
 from django.test import Client
 
