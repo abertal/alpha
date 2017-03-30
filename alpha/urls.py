e95fffc508468f23741d3856b441bb0cdd835ca7
ivan <ivan@localhost.localdomain>
2017-03-30 10:34:04 +0200

    Anade media urls

--- alpha/urls.py
+++ alpha/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import RedirectView
@@ -24,3 +26,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^webapp/', include(webapp.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns = [
+        # ... the rest of your URLconf goes here ...
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
