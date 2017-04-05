c2541ccc983ae680220a1b2e6e6d2007099aab70
yago <benitezdemiguel@gmail.com>
2017-04-05 11:51:23 +0200

    Combinado filtros (apellido y nombre)

--- webapp/filters.py
+++ webapp/filters.py
@@ -1,12 +1,17 @@
+from django.db.models import Q
+
 import django_filters
+
 from core.models import Person
 
 
 class PersonFilter(django_filters.FilterSet):
 
+    q = django_filters.CharFilter(name='name', method='custom_filter')
+
     class Meta:
         model = Person
-        fields = {
-            'surname': ['contains'],
-            'name': ['contains'],
-        }
+        fields = []
+
+    def custom_filter(self, queryset, name, value):
+        return queryset.filter(Q(name__icontains=value) | Q(surname__icontains=value))
