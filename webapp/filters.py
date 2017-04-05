fc6eb4397fe314703b63e8499940e301e4d35967
Ivan Souto <isgroba@gmail.com>
2017-04-05 08:58:23 +0200

    Agrega listado voluntarios

--- webapp/filters.py
+++ webapp/filters.py
@@ -1,5 +1,6 @@
 import django_filters
 from core.models import Person
+from core.models import Volunteer
 
 
 class PersonFilter(django_filters.FilterSet):
@@ -7,3 +8,9 @@ class PersonFilter(django_filters.FilterSet):
     class Meta:
         model = Person
         fields = ['name']
+
+class VolunteerFilter(django_filters.FilterSet):
+
+    class Meta:
+        model = Volunteer
+        fields = ['person__name']
