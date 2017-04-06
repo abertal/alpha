81f389917a7423db467b9e91a01566c51bda0b08
Ivan Nieto <ivan.n.s@tuta.io>
2017-04-06 19:12:42 +0200

    Prepara urls i18n

--- alpha/urls.py
+++ alpha/urls.py
@@ -15,18 +15,19 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import RedirectView
-
 
 import webapp.urls
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='webapp/')),
     url(r'^admin/', admin.site.urls),
-    url(r'^webapp/', include(webapp.urls)), ] + \
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^webapp/', include(webapp.urls)),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:  # pragma: nocover
     import debug_toolbar
