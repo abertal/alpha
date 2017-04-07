42d37b691cb3ac4e4a2dfe08584cd99beb170961
yago <benitezdemiguel@gmail.com>
2017-04-07 11:04:05 +0200

    Refactorizacion 1 tests filtros

--- webapp/tests/conftest.py
+++ webapp/tests/conftest.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.test import Client
 
 import pytest
+from django_filters import Filter
 
 from core import models
 
@@ -14,7 +15,8 @@ def person():
 @pytest.fixture
 def logged_client():
     c = Client()
-    user = User.objects.create_user('user00', 'first.last@example.com', 'secret')
+    user = User.objects.create_user(
+        'user00', 'first.last@example.com', 'secret')
     c.force_login(user)
     return c
 
@@ -22,3 +24,67 @@ def logged_client():
 @pytest.fixture
 def person_filter():
     return models.Person.objects.create(name='Example', surname='SurExample')
+
+
+@pytest.fixture
+def person_query_filter():
+    person_filter()
+    return models.Person.objects.all().order_by('name')
+
+
+@pytest.fixture
+def volunteer_filter():
+    person_filter()
+    return models.Volunteer.objects.create(person=models.Person.objects.get(name='Example'))
+
+
+@pytest.fixture
+def volunteer_query_filter():
+    volunteer_filter()
+    return models.Volunteer.objects.all().order_by('id')
+
+
+@pytest.fixture
+def recipient_filter():
+    person_filter()
+    return models.Recipient.objects.create(person=models.Person.objects.get(name='Example'))
+
+
+@pytest.fixture
+def recipient_query_filter():
+    recipient_filter()
+    return models.Recipient.objects.all().order_by('id')
+
+
+@pytest.fixture
+def membership():
+    return models.Membership.objects.create(membership_fee='2',
+                                            type_of_membership='Infantil')
+
+
+@pytest.fixture
+def member_filter():
+    person_filter()
+    membership()
+    return models.Member.objects.create(
+                                        person=models.Person.objects.get(name='Example'),
+                                        membership=models.Membership.objects.get(
+                                            type_of_membership='Infantil'))
+
+
+@pytest.fixture
+def member_query_filter():
+    member_filter()
+    return models.Member.objects.all().order_by('id')
+
+
+@pytest.fixture
+def filter_name():
+    f = Filter(name='name', lookup_expr='icontains')
+    return f
+
+
+@pytest.fixture
+def filter_person_name():
+    f = Filter(name='person__name', lookup_expr='icontains')
+    return f
--- webapp/tests/test_filters.py
+++ webapp/tests/test_filters.py
@@ -1,11 +1,9 @@
 from django.test import TransactionTestCase
 
 import pytest
-from django_filters import Filter
 
-from core.models import Member, Membership, Person, Recipient, Volunteer
+import webapp.tests.conftest as fixture
 from webapp.filters import MemberFilter, PersonFilter, RecipientFilter, VolunteerFilter
-from webapp.tests.conftest import person_filter
 
 
 class FilterTests(TransactionTestCase):
@@ -13,47 +11,28 @@ class FilterTests(TransactionTestCase):
     @pytest.mark.django_db
     def test_filtering(self):
         t = TransactionTestCase()
-        person_filter()
-        qs = Person.objects.all().order_by('name')
-        f = Filter(name='name', lookup_expr='icontains')
-        result = f.filter(qs, 'ex')
+        qs = fixture.person_query_filter()
+        result = fixture.filter_name().filter(qs, 'ex')
         qs2 = PersonFilter.custom_filter(self, qs, '', 'ex')
         t.assertQuerysetEqual(qs2, [repr(r) for r in result])
 
     def test_filter_volunteer(self):
         t = TransactionTestCase()
-        p1 = Person(name='Example')
-        p1.save()
-        v1 = Volunteer(person=p1)
-        v1.save()
-        qs = Volunteer.objects.all().order_by('id')
-        f = Filter(name='person__name', lookup_expr='icontains')
-        result = f.filter(qs, 'ex')
+        qs = fixture.volunteer_query_filter()
+        result = fixture.filter_person_name().filter(qs, 'ex')
         qs2 = VolunteerFilter.custom_filter(self, qs, '', 'ex')
         t.assertQuerysetEqual(qs2, [repr(r) for r in result])
 
     def test_filter_recipient(self):
         t = TransactionTestCase()
-        p1 = Person(name='Example')
-        p1.save()
-        r1 = Recipient(person=p1)
-        r1.save()
-        qs = Recipient.objects.all().order_by('id')
-        f = Filter(name='person__name', lookup_expr='icontains')
-        result = f.filter(qs, 'ex')
+        qs = fixture.recipient_query_filter()
+        result = fixture.filter_person_name().filter(qs, 'ex')
         qs2 = RecipientFilter.custom_filter(self, qs, '', 'ex')
         t.assertQuerysetEqual(qs2, [repr(r) for r in result])
 
     def test_filter_member(self):
         t = TransactionTestCase()
-        p1 = Person(name='Example')
-        p1.save()
-        mship = Membership(membership_fee=2)
-        mship.save()
-        m1 = Member(person=p1, membership=mship)
-        m1.save()
-        qs = Member.objects.all().order_by('id')
-        f = Filter(name='person__name', lookup_expr='icontains')
-        result = f.filter(qs, 'ex')
+        qs = fixture.member_query_filter()
+        result = fixture.filter_person_name().filter(qs, 'ex')
         qs2 = MemberFilter.custom_filter(self, qs, '', 'ex')
         t.assertQuerysetEqual(qs2, [repr(r) for r in result])
