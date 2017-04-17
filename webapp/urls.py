4ba29fd4d047e2e06961066cc89accea7f6df7fb
gvame <gvarelamendez@gmail.com>
2017-04-17 11:36:54 +0200

    modifica url group

--- webapp/urls.py
+++ webapp/urls.py
@@ -16,7 +16,7 @@ person = [
 
 group = [
     url(r'^(?P<pk>\d+)/edit/$', views.GroupEdit.as_view(), name='group-edit'),
-    url(r'^/group/$', views.GroupCreate.as_view(), name='group-create')
+    url(r'^new/$', views.GroupCreate.as_view(), name='group-create'),
 ]
 
 recipient = [
