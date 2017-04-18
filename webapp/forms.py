39e8a871f1b4266f58535a78ee356aeed1899c34
gvame <gvarelamendez@gmail.com>
2017-04-18 16:33:54 +0200

    class RecipientGroup - forms.py

--- webapp/forms.py
+++ webapp/forms.py
@@ -55,6 +55,10 @@ class RecipientEdit(forms.ModelForm):
         model = models.Recipient
         fields = 'category',
 
+class RecipientGroup(forms.ModelForm):
+    class Meta:
+        model = models.Group
+        fields = 'group_name'
 
 class VolunteerCreate(forms.ModelForm):
     class Meta:
