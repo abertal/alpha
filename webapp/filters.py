b2fcc855ca69d33fae459e6117e496206c0d9edd
yago <benitezdemiguel@gmail.com>
2017-04-24 13:12:11 +0200

    Concat queries

--- webapp/filters.py
+++ webapp/filters.py
@@ -1,6 +1,6 @@
-from django.db.models import Q
+from django.db.models import Q, Value as V
 from django.utils.translation import ugettext_lazy as _
-
+from django.db.models.functions import Concat
 import django_filters
 
 from core.models import Group
@@ -11,7 +11,8 @@ class PersonFilter(django_filters.FilterSet):
     q = django_filters.CharFilter(label=_('Nombre'), name='name', method='custom_filter')
 
     def custom_filter(self, queryset, name, value):
-        return queryset.filter(Q(name__icontains=value) | Q(surname__icontains=value))
+        combined_name=Concat(name,V(' '),surname)
+        return queryset.filter(Q(name__icontains=value)).annotate(' ').filter(Q(surname_icontains=value))
 
 
 class FromPersonFilter(django_filters.FilterSet):
