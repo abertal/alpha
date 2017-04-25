ae131f41815021c4c5ee60292ed06d232d98b69e
gvame <gvarelamendez@gmail.com>
2017-04-25 19:55:22 +0200

    divisiones de cursos + definiciones

--- core/models.py
+++ core/models.py
@@ -67,8 +67,40 @@ class Recipient(models.Model):
         ('child', _('Infantil')),
         ('juvenile', _('Juvenil')),
     ]
-    estudies = models.TextField(
-        verbose_name=_('Estudios'), blank=True, default='')
+
+    INFANTILE_COURSES = [
+        ('4EI', _('4 Educacion Infantil')),
+        ('5EI', _('5 Educacion Infantil')),
+        ('6EI', _('6 Educacion Infantil')),
+    ]
+
+    PRIMARY_COURSES = [
+        ('1EP', _('1 Educacion Primaria')),
+        ('2EP', _('2 Educacion Primaria')),
+        ('3EP', _('3 Educacion Primaria')),
+        ('4EP', _('4 Educacion Primaria')),
+        ('5EP', _('5 Educacion Primaria')),
+        ('6EP', _('6 Educacion Primaria')),
+    ]
+
+    SECONDARY_COURSES = [
+        ('1ESO', _('1 Educación Secundaria Obligatoria')),
+        ('2ESO', _('2 Educación Secundaria Obligatoria')),
+        ('3ESO', _('3 Educación Secundaria Obligatoria')),
+        ('4ESO', _('4 Educación Secundaria Obligatoria')),
+    ]
+
+    POSTSECONDARY_COURSES = [
+        ('1BACH', _('1 Bachillerato')),
+        ('2BACH', _('2 Bachillerato')),
+        ('FP', _('Formación Profesional')),
+        ('CARRERA', _('Carrera Universitaria')),
+    ]
+
+    infantile_courses = models.CharField(_('Estudios Infantiles'), choices=INFANTILE_COURSES, max_legth=32)
+    primary_courses = models.CharField(_('Estudios Primarios'), choices=PRIMARY_COURSES, max_legth=32)
+    secondary_courses = models.CharField(_('Estudios Secundarios'), choices=SECONDARY_COURSES, max_legth=32)
+    postsecondary_courses = models.CharField(_('Estudios Superiores'), choices=POSTSECONDARY_COURSES, max_legth=32)
     sibling = models.IntegerField(
         verbose_name=_('Hermanos'), blank=True, null=True, default=None)
     authorize_photo = models.TextField(
