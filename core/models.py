93d2846e191cf9a4dd6618588a6edde358d2654e
edgar <edgaralcantarafernandez27@gmail.com>
2017-05-24 09:50:53 +0200

    Añade verbose_name

--- core/models.py
+++ core/models.py
@@ -160,7 +160,7 @@ class Group(models.Model):
         verbose_name = _('Grupo')
 
     group_name = models.TextField(verbose_name=_('Nombre grupo'))
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, default=None, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, default=None, null=True, verbose_name=_('Proyecto'))
 
     def __str__(self):
         return '{}'.format(self.id)
