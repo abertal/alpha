66c3fd0a35796583cfc9852bc5dab9bc05c4e23c
Miguel Gonzalez <migonzalvar@gmail.com>
2017-07-16 19:39:16 +0200

    Vendoriza jquery y tether

--- alpha/settings.py
+++ alpha/settings.py
@@ -162,6 +162,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'vendor', 'bootstrap-4.0.0-alpha.6'),
+    os.path.join(BASE_DIR, 'vendor', 'jquery-3.2.1'),
+    os.path.join(BASE_DIR, 'vendor', 'tether-1.3.3'),
     os.path.join(BASE_DIR, 'vendor', 'vue'),
 ]
 
