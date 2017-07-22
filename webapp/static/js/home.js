f5a45ce6f30f0f3cd71ec8e6dee5cc9457f25447
Miguel Gonzalez <migonzalvar@gmail.com>
2017-07-22 20:15:03 +0200

    (WIP) Componente para seleccionar persona

---
+++ webapp/static/js/home.js
@@ -0,0 +1,31 @@
+
+var data = {
+  loading: false,
+  response: null,
+  person: null,
+  results: null
+}
+
+/* eslint-disable no-new */
+/* eslint-disable no-undef */
+var vm = new Vue({
+  el: '#app',
+  data: data,
+  methods: {
+    clickSearch: function (message) {
+      var el = message.target
+      el.setAttribute('value', 'Buscando...')
+      vm.results = []
+      vm.loading = true
+      axios.get('//httpbin.org/delay/3')
+      .then(function (response) {
+        vm.response = response
+        console.dir(response)
+        vm.results = [{name: 'A'}, {name: 'B'}]
+        el.setAttribute('value', 'Buscar')
+        vm.loading = false
+      })
+    }
+  },
+  delimiters: ['${', '}']
+})
