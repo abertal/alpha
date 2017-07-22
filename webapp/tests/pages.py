e3b79ef08e1fe7e30f6ad394360616533aa46386
Miguel Gonzalez <migonzalvar@gmail.com>
2017-07-22 23:12:17 +0200

    Elimina import no usado

--- webapp/tests/pages.py
+++ webapp/tests/pages.py
@@ -1,5 +1,4 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
