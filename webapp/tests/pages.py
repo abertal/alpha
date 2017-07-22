0316b37e63cc94356576a84229404a233fc403c9
Miguel Gonzalez <migonzalvar@gmail.com>
2017-07-22 17:59:26 +0200

    Aplica isort

--- webapp/tests/pages.py
+++ webapp/tests/pages.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 def w(driver, locator):
