c7455115742bf0437f2bf8aef5d76b66b81500cf
Miguel Gonzalez <migonzalvar@gmail.com>
2017-07-26 21:00:34 +0200

    Reindenta

--- webapp/assets/js/components/PersonCard.vue
+++ webapp/assets/js/components/PersonCard.vue
@@ -1,16 +1,16 @@
 <template>
-    <li class="list-group-item">
-        {{ person.name }} {{ person.surname }}
-        <button v-on:click="select(person)">Seleccionar</button>
-    </li>
+  <li class="list-group-item">
+    {{ person.name }} {{ person.surname }}
+    <button v-on:click="select(person)">Seleccionar</button>
+  </li>
 </template>
 <script>
-    export default {
-        props: ['person'],
-        methods: {
-            select: function (person) {
-                this.$emit('selected', person.id)
-            }
-        }
+  export default {
+    props: ['person'],
+    methods: {
+      select: function (person) {
+        this.$emit('selected', person.id)
+      }
     }
+  }
 </script>
