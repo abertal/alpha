c6c2544ee70ba5cfbd8bc407a1419ae45cc66df4
Miguel Gonzalez <migonzalvar@gmail.com>
2017-08-05 21:54:04 +0200

    Añade bootstrap-vue

--- webapp/src/alpha.js
+++ webapp/src/alpha.js
@@ -1,1 +1,9 @@
+// This file is used to gather all assets. They will
+// be included in all HTML pages as globals
 import './assets/alpha.less'
+
+// Following CSS area included explicitly in all HTML pages
+// using version from dist/ folder.
+//
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
