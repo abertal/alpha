904e67b6b7df99b5924fb07f4dd22387cba192a9
Miguel Gonzalez <migonzalvar@gmail.com>
2017-09-11 19:48:47 +0200

    Añade JS bundles tal cual

--- webapp/static/js/home.bundle.js
+++ webapp/static/js/home.bundle.js
@@ -12740,7 +12740,7 @@ exports = module.exports = {
       // some components may have related popup item, on which we shall prevent the click outside event handler.
       var elements = e.path || (e.composedPath && e.composedPath())
       elements && elements.length > 0 && elements.unshift(e.target)
-
+      
       if (el.contains(e.target) || isPopup(vNode.context.popupItem, elements)) return
 
       el.__vueClickOutside__.callback(e)
@@ -12757,7 +12757,7 @@ exports = module.exports = {
   update: function (el, binding) {
     if (validate(binding)) el.__vueClickOutside__.callback = binding.value
   },
-
+  
   unbind: function (el, binding, vNode) {
     // Remove Event Listeners
     !isServer(vNode) && document.removeEventListener('click', el.__vueClickOutside__.handler)
