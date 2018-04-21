a233d2bc97b684b6877d283cd0832280b2448bd7
Miguel Gonzalez <migonzalvar@gmail.com>
2018-04-21 16:16:10 +0200

    ci: Actualiza Nu Html Checker (v.Nu)

--- html5validation/install.sh
+++ html5validation/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
-wget https://github.com/validator/validator/releases/download/17.7.0/vnu.jar_17.7.0.zip -O ./vnu.jar.zip
+wget https://github.com/validator/validator/releases/download/18.3.0/vnu.jar_18.3.0.zip -O ./vnu.jar.zip
 unzip -op vnu.jar.zip dist/vnu.jar > vnu.jar
